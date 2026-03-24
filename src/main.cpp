bbfd136fc1e5cdf65c4fe0319477c3e51360cf45
Raymond Walker <raymond.j.walker@protonmail.com>
2026-03-24 09:08:12 -0400

    wifi Connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,36 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <SPI.h>
 #include <freertos/FreeRTOS.h>
+#include "secrets.h"
 
+
+int status = WL_IDLE_STATUS;
 
 void setup() {
-  // put your setup code here, to run once:
-  
+    Serial.begin(115200);
+    delay(1000); 
+
+    Serial.print("Connecting to: ");
+    Serial.println(WIFI_SSID);
+
+    // Start the connection process
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+    // Poll WiFi.status() until it returns WL_CONNECTED
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected!");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
+
 }
